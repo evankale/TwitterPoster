866635a36db77f0c91172c7e8307a87950b8dc63
Evan Kale <iseedeadpixel1337@gmail.com>
2015-04-02 17:15:40 -0400

    initial commit

---
+++ src/com/isdp/twitterposter/TwitterManager.java
@@ -0,0 +1,217 @@
+/*
+ * Copyright (c) 2015 Evan Kale
+ * Email: EvanKale91@gmail.com
+ * Website: www.ISeeDeadPixel.com
+ *
+ * This file is part of TwitterPoster.
+ *
+ * TwitterPoster is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.isdp.twitterposter;
+
+import java.util.StringTokenizer;
+
+import twitter4j.FilterQuery;
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class TwitterManager
+{
+	private static TwitterManager instance;
+
+	private TwitterManager()
+	{
+	}
+
+	public static TwitterManager getInstance()
+	{
+		if (instance == null)
+			instance = new TwitterManager();
+
+		return instance;
+	}
+
+	/*
+	 * Get Twitter OAuth keys here:
+	 * https://apps.twitter.com/
+	 *
+	 */
+
+	private static final String OAUTH_CONSUMER_KEY = "<YOUR CONSUMER KEY HERE>";
+	private static final String OAUTH_CONSUMER_SECRET = "<YOUR CONSUMER SECRET HERE>";
+	private static final String OAUTH_ACCESS_TOKEN = "<YOUR ACCESS TOKEN HERE>";
+	private static final String OAUTH_ACCESS_TOKEN_SECRET = "<YOUR ACCESS TOKEN SECRET HERE>";
+	private static final long FOLLOW_ID = 0L; //TWITTER ID OF ACCOUNT TO LISTEN TO (numeric, not hastag)
+
+	public static final int TWITTER_CHARACTER_LIMIT = 140;
+
+	private Configuration configuration;
+
+	public void initTwitter()
+	{
+		ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+		configurationBuilder.setDebugEnabled(true).setOAuthConsumerKey(OAUTH_CONSUMER_KEY)
+				.setOAuthConsumerSecret(OAUTH_CONSUMER_SECRET).setOAuthAccessToken(OAUTH_ACCESS_TOKEN)
+				.setOAuthAccessTokenSecret(OAUTH_ACCESS_TOKEN_SECRET);
+		configuration = configurationBuilder.build();
+
+	}
+
+	public void tweet(String message)
+	{
+		TwitterFactory tf = new TwitterFactory(configuration);
+		Twitter twitter = tf.getInstance();
+		try
+		{
+			Status status = twitter.updateStatus(message);
+			System.out.println("Tweet status: Successfully updated the status to [" + status.getText() + "].");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public void startTwitterStream()
+	{
+		TwitterStream twitterStream = new TwitterStreamFactory(configuration).getInstance();
+
+		StatusListener listener = new StatusListener()
+		{
+			@Override
+			public void onStatus(Status status)
+			{
+				try
+				{
+					System.out.println("Text recieved: @" + status.getUser().getScreenName() + " - " + status.getText()
+							+ "\n");
+
+					StringTokenizer st = new StringTokenizer(status.getText(), " ");
+
+					//first token indicates search engine
+					String searchEngine = st.nextToken();
+
+					//burn the next random token
+					String randToken = st.nextToken();
+
+					//next token indicates max number of results to return
+					int maxResults = Integer.parseInt(st.nextToken());
+
+					String searchString = "";
+					while (st.hasMoreTokens())
+					{
+						searchString += st.nextToken() + " ";
+					}
+
+					if (searchEngine.equals(GoogleManager.TAG_YELP))
+					{
+						String[] results = GoogleManager.getInstance().trySearch(searchString,
+								GoogleManager.SEARCH_YELP);
+
+						if (results != null)
+						{
+							for (int i = 0; i < results.length && i < maxResults; ++i)
+							{
+								String tweetMsg = Util.truncateString(results[i], TWITTER_CHARACTER_LIMIT);
+
+								System.out.println("Tweeting" + tweetMsg);
+
+								tweet(tweetMsg);
+							}
+						}
+						else if (results == null || results.length == 0)
+						{
+							tweet(Util.generateRandomString(7) + "\n" + "No results found!");
+						}
+					}
+					else if (searchEngine.equals(GoogleManager.TAG_WIKI))
+					{
+						String[] results = GoogleManager.getInstance().trySearch(searchString,
+								GoogleManager.SEARCH_WIKI);
+
+						if (results != null)
+						{
+							for (int i = 0; i < results.length && i < maxResults; ++i)
+							{
+								String tweetMsg = Util.generateRandomString(3) + "\n" + Util.shortenText(results[i]);
+								tweetMsg = Util.truncateString(tweetMsg, TWITTER_CHARACTER_LIMIT);
+
+								System.out.println("Tweeting " + tweetMsg);
+
+								tweet(tweetMsg);
+							}
+						}
+						else if (results == null || results.length == 0)
+						{
+							tweet(Util.generateRandomString(7) + "\n" + "No results found!");
+						}
+					}
+				}
+				catch (Exception e)
+				{
+				}
+			}
+
+			@Override
+			public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice)
+			{
+				System.out.println("onDeletionNotice: Got a status deletion notice id:"
+						+ statusDeletionNotice.getStatusId());
+			}
+
+			@Override
+			public void onTrackLimitationNotice(int numberOfLimitedStatuses)
+			{
+				System.out.println("onTrackLimitationNotice: Got track limitation notice:" + numberOfLimitedStatuses);
+			}
+
+			@Override
+			public void onScrubGeo(long userId, long upToStatusId)
+			{
+				System.out
+						.println("onScrubGeo: Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+			}
+
+			@Override
+			public void onStallWarning(StallWarning warning)
+			{
+				System.out.println("onStallWarning: Got stall warning:" + warning);
+			}
+
+			@Override
+			public void onException(Exception ex)
+			{
+				System.out.println("onException" + ex.toString());
+			}
+		};
+
+		twitterStream.addListener(listener);
+
+		FilterQuery tweetFilterQuery = new FilterQuery();
+
+		tweetFilterQuery.follow(new long[]
+		{ FOLLOW_ID });
+
+		twitterStream.filter(tweetFilterQuery);
+	}
+}
---
+++ src/com/isdp/twitterposter/Util.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2015 Evan Kale
+ * Email: EvanKale91@gmail.com
+ * Website: www.ISeeDeadPixel.com
+ *
+ * This file is part of TwitterPoster.
+ *
+ * TwitterPoster is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.isdp.twitterposter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class Util
+{
+	private static Random rand;
+
+	public static String generateRandomString(int length)
+	{
+		if (rand == null)
+			rand = new Random();
+
+		String retStr = "";
+
+		int range = 'z' - 'a';
+
+		for (int i = 0; i < length; ++i)
+		{
+			int randInRange = rand.nextInt(range);
+
+			if (rand.nextFloat() < 0.5f)
+			{
+				retStr += (char) ('a' + randInRange);
+			}
+			else
+			{
+				retStr += (char) ('A' + randInRange);
+			}
+		}
+
+		return retStr;
+	}
+
+	public static String convertInputStreamToString(InputStream inputStream) throws IOException
+	{
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+		String line = "";
+		String result = "";
+		while ((line = bufferedReader.readLine()) != null)
+			result += line;
+
+		inputStream.close();
+		return result;
+	}
+
+	public static String shortenText(String str)
+	{
+		String text = "";
+		boolean spaced = false;
+		for (int i = 0; i < str.length(); ++i)
+		{
+			if (str.charAt(i) == ' ' || str.charAt(i) == '\n' || str.charAt(i) == '\t')
+			{
+				spaced = true;
+			}
+			else
+			{
+				if (spaced)
+				{
+					text += (str.charAt(i) + "").toUpperCase();
+					spaced = false;
+				}
+				else
+				{
+					text += str.charAt(i);
+				}
+			}
+		}
+		return text;
+	}
+
+	public static String truncateString(String str, int length)
+	{
+		int minLength = Math.min(length, str.length());
+		return str.substring(0, minLength);
+	}
+}
